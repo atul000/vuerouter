50c2b6655a5be817254ae68ef8182e44dfaafb47
atul beniwal <33995896+atul000@users.noreply.github.com>
2020-01-13 20:26:07 +0530

    router

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,24 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <Navbar />
+      <!-- <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> -->
     </div>
     <router-view />
   </div>
 </template>
+
+<script>
+import Navbar from "@/components/Navbar";
+
+export default {
+  name: "App",
+  components: {
+    Navbar
+  }
+};
+</script>
 
 <style>
 #app {
--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,15 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script>
+export default {
+  name: "About",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
